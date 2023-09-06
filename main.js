3540324cc44e49d7d42392e03c556e07a209b291
Ruslan Aliyev <ruslanra@code.edu.az>
2023-09-07 00:19:11 +0400

    trying

---
+++ main.js
@@ -0,0 +1,9 @@
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const specialChars = ["%", "*", "/", "-", "+","="];
+const result = document.querySelector(".result");
+
+
+
+
+
