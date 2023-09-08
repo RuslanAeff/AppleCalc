dc8181e4af405d66545625dde6d284bd401720de
Ruslan Aliyev <ruslanra@code.edu.az>
2023-09-09 02:16:25 +0400

    added js code

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
-const display = document.querySelector(".display");
-const buttons = document.querySelectorAll("button");
-const specialChars = ["%", "*", "/", "-", "+","="];
-const result = document.querySelector(".result");
+const display = document.querySelector('.result');
+const buttons = document.querySelectorAll('.button');
+let currentInput = '';
+
+// Add an event listener to each button
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const value = button.dataset.value;
+
+        if (value === 'AC') {
+            currentInput = '';
+        } else if(value ==='+/-'){
+            currentInput= eval("-1*"+currentInput)
+        }
+        else if(value === '%'){
+            currentInput=eval(currentInput/100)
+        }
+        else if (value === '=') {
+            try {
+                currentInput = eval(currentInput).toString();
+            } catch (error) {
+                currentInput = 'Error';
+            }
+        } else {
+            currentInput += value;
+        }
+
+        display.textContent = currentInput;
+    });
+});
 
 
 
